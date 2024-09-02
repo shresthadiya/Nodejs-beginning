b95dc6749ccfcf0d7f44e144fc788d3f9abc647d
Diya Shrestha <diyashrestha2001@gmail.com>
2024-09-02 15:57:40 +0545

    people.js and modules.js files modified

--- modules.js
+++ modules.js
@@ -1,1 +1,11 @@
-const xyz = require('./people');
+// const xyz = require('./people');
+// console.log(xyz.people);
+
+// const {people} = require('./people');
+// console.log(people);
+
+const {people, ages} = require('./people');
+console.log(people, '\n', ages, '\n');
+
+const os = require('os');
+console.log(os.platform(), '\n', os.homedir());
--- people.js
+++ people.js
@@ -1,3 +1,10 @@
 const people = ['Apple', 'Banana', 'Carrot', 'Dumplings'];
+const ages = [12, 31, 25, 19];
+// console.log(people);
 
-console.log(people);
+// module.exports = people;
+// module.exports = {people};
+
+module.exports = {people, ages};
+
+// module.exports = 'Hello';
